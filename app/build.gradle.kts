57b0ea95f9b4c0393bc2a24c554af6383c6538e0
Emily Ehlert <ehlertto@hu-berlin.de>
2025-05-20 13:23:19 +0200

    Add MTLS client authentication

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -119,6 +119,7 @@ dependencies {
     implementation(libs.androidx.core.splashscreen)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.activity)
+    implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.fragment)
     coreLibraryDesugaring(libs.androiddesugarlibs)
 
--- app/src/main/java/org/jellyfin/mobile/app/AppModule.kt
+++ app/src/main/java/org/jellyfin/mobile/app/AppModule.kt
@@ -5,7 +5,7 @@ import androidx.core.net.toUri
 import coil.ImageLoader
 import com.google.android.exoplayer2.database.DatabaseProvider
 import com.google.android.exoplayer2.database.StandaloneDatabaseProvider
-import com.google.android.exoplayer2.ext.cronet.CronetDataSource
+import com.google.android.exoplayer2.ext.okhttp.OkHttpDataSource
 import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory
 import com.google.android.exoplayer2.extractor.ts.TsExtractor
 import com.google.android.exoplayer2.source.DefaultMediaSourceFactory
@@ -16,17 +16,13 @@ import com.google.android.exoplayer2.source.hls.HlsMediaSource
 import com.google.android.exoplayer2.upstream.DataSource
 import com.google.android.exoplayer2.upstream.DataSpec
 import com.google.android.exoplayer2.upstream.DefaultDataSource
-import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
 import com.google.android.exoplayer2.upstream.ResolvingDataSource
 import com.google.android.exoplayer2.upstream.cache.Cache
 import com.google.android.exoplayer2.upstream.cache.CacheDataSource
 import com.google.android.exoplayer2.upstream.cache.NoOpCacheEvictor
 import com.google.android.exoplayer2.upstream.cache.SimpleCache
-import com.google.android.exoplayer2.util.Util
 import kotlinx.coroutines.channels.Channel
 import okhttp3.OkHttpClient
-import org.chromium.net.CronetEngine
-import org.chromium.net.CronetProvider
 import org.jellyfin.mobile.MainViewModel
 import org.jellyfin.mobile.bridge.NativePlayer
 import org.jellyfin.mobile.downloads.DownloadsViewModel
@@ -41,6 +37,7 @@ import org.jellyfin.mobile.setup.ConnectionHelper
 import org.jellyfin.mobile.utils.Constants
 import org.jellyfin.mobile.utils.PermissionRequestHelper
 import org.jellyfin.mobile.utils.extractId
+import org.jellyfin.mobile.utils.getClientCertificate
 import org.jellyfin.mobile.utils.isLowRamDevice
 import org.jellyfin.mobile.webapp.RemoteVolumeProvider
 import org.jellyfin.mobile.webapp.WebViewFragment
@@ -53,15 +50,56 @@ import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import java.io.File
-import java.util.concurrent.Executors
+import java.security.KeyStore
+import javax.net.ssl.KeyManagerFactory
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManagerFactory
+import javax.net.ssl.X509TrustManager
 
 const val PLAYER_EVENT_CHANNEL = "PlayerEventChannel"
-private const val HTTP_CACHE_SIZE: Long = 16 * 1024 * 1024
 private const val TS_SEARCH_PACKETS = 1800
 
 val applicationModule = module {
     single { AppPreferences(androidApplication()) }
-    single { OkHttpClient() }
+    single<OkHttpClient> {
+        val okHttpBuilder = OkHttpClient.Builder()
+
+        get<AppPreferences>().currentServerId?.let { id ->
+            getClientCertificate(id.toString())?.let { mtls ->
+                val keyStore = KeyStore.getInstance("PKCS12")
+                keyStore.load(null, null)
+                keyStore.setKeyEntry(
+                    "client",
+                    mtls.privateKey,
+                    null,
+                    mtls.certificateChain,
+                )
+
+                val kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm())
+                kmf.init(keyStore, null)
+
+                val sslContext = SSLContext.getInstance("TLS").apply {
+                    init(kmf.keyManagers, null, null)
+                }
+
+                val trustManagerFactory = TrustManagerFactory.getInstance(
+                    TrustManagerFactory.getDefaultAlgorithm(),
+                ).apply {
+                    init(null as KeyStore?) // use default trust store
+                }
+
+                val trustManager = trustManagerFactory.trustManagers
+                    .filterIsInstance<X509TrustManager>()
+                    .firstOrNull()
+
+                trustManager?.let {
+                    okHttpBuilder.sslSocketFactory(sslContext.socketFactory, it)
+                }
+            }
+        }
+
+        okHttpBuilder.build()
+    }
     single { ImageLoader(androidApplication()) }
     single { PermissionRequestHelper() }
     single { RemoteVolumeProvider(get()) }
@@ -111,25 +149,7 @@ val applicationModule = module {
         val context: Context = get()
         val apiClient: ApiClient = get()
 
-        val provider = CronetProvider.getAllProviders(context).firstOrNull { provider: CronetProvider ->
-            (provider.name == CronetProvider.PROVIDER_NAME_APP_PACKAGED) && provider.isEnabled
-        }
-
-        val baseDataSourceFactory = if (provider != null) {
-            val cronetEngine = provider.createBuilder()
-                .enableHttp2(true)
-                .enableQuic(true)
-                .enableBrotli(true)
-                .enableHttpCache(CronetEngine.Builder.HTTP_CACHE_IN_MEMORY, HTTP_CACHE_SIZE)
-                .build()
-            CronetDataSource.Factory(cronetEngine, Executors.newCachedThreadPool()).apply {
-                setUserAgent(Util.getUserAgent(context, Constants.APP_INFO_NAME))
-            }
-        } else {
-            DefaultHttpDataSource.Factory().apply {
-                setUserAgent(Util.getUserAgent(context, Constants.APP_INFO_NAME))
-            }
-        }
+        val baseDataSourceFactory = OkHttpDataSource.Factory(get<OkHttpClient>())
 
         val dataSourceFactory = DefaultDataSource.Factory(context, baseDataSourceFactory)
 
